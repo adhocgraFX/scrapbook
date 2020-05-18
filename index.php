307d557abf116a1eb9a37782ca9ef589c650c16d
adhocgraFX <info@adhocgrafx.de>
2020-05-18 19:23:13 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.carcass
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
+/** @var JDocumentHtml $this */
+
+$app  = Factory::getApplication();
+$lang = $app->getLanguage();
+$wa   = $this->getWebAssetManager();
+
+// Detecting Active Variables
+$option    = $app->input->getCmd('option', '');
+$view      = $app->input->getCmd('view', '');
+$layout    = $app->input->getCmd('layout', '');
+$task      = $app->input->getCmd('task', '');
+$itemid    = $app->input->getCmd('Itemid', '');
+$sitename  = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
+$menu      = $app->getMenu()->getActive();
+$pageclass = $menu->params->get('pageclass_sfx');
+$tpath     = $this->baseurl . '/templates/' . $this->template;
+
+// get template params
+// logo
+$logo = $this->params->get('logo');
+// header style
+$headerbackground = $this->params->get('headerbackground');
+// call to action
+$buttontext = $this->params->get('buttontext');
+$buttonlink = $this->params->get('buttonlink');
+
+
+
+// Enable assets
+$wa->enableAsset('template.carcass.base');
+
+// meta data
+$this->setMetaData('viewport', 'width=device-width, initial-scale=1');
+?>
+
+<!DOCTYPE html>
+
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+
+<head>
+
+    <jdoc:include type="metas"/>
+    <jdoc:include type="styles"/>
+    <jdoc:include type="scripts"/>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon.png">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- load css options -->
+	<?php include_once('css/template.css.php'); ?>
+</head>
+
+<body class=" <?php echo $pageclass ?>
+              <?php echo $option . ' view-' . $view . ($layout ? ' layout-' . $layout : ' no-layout') . ($task ? ' task-' . $task : ' no-task'); ?>
+              <?php echo($itemid ? ' itemid-' . $itemid : ''); ?>" role="document">
+
+<header class="app-bar" role="banner">
+	<?php if ($logo): ?>
+        <div class="logo-image">
+            <a href="<?php echo $this->baseurl ?>">
+                <img src="<?php echo $this->baseurl ?>/<?php echo htmlspecialchars($logo); ?>"
+                     alt="<?php echo htmlspecialchars($sitename); ?>"/>
+            </a>
+        </div>
+	<?php endif; ?>
+
+    <section class="app-bar-container">
+		<h1 class="logo-text">
+            <a href="<?php echo $this->baseurl ?>"><?php echo htmlspecialchars($sitename); ?></a>
+        </h1>
+		<?php if ($this->countModules('sidebar')): ?>
+            <button class="sidebar-menu" aria-label="Navigation"></button>
+		<?php endif; ?>
+    </section>
+</header>
+
+<section class="layout block-group">
+    <section class="main-container">
+
+        <aside class="sidebar-container" role="complementary">
+            <h4 class="sidebar-text">Sidebar</h4>
+
+	        <?php if ($this->countModules('search')): ?>
+                <div class="search-container">
+                    <jdoc:include type="modules" name="search" style=" none"/>
+                </div>
+	        <?php endif; ?>
+
+	        <?php if ($this->countModules('nav')): ?>
+                <nav class="nav-container" id="navdrawer" role="navigation">
+                    <div class="module-title">
+                        <h4 class="title">Menu</h4>
+                    </div>
+                    <jdoc:include type="modules" name="nav"/>
+                </nav    >
+            <?php endif; ?>
+
+		    <?php if ($this->countModules('sidebar')): ?>
+                <jdoc:include type="modules" name="sidebar" style="jduo"/>
+		    <?php endif; ?>
+
+		    <?php if ($this->countModules('footer')): ?>
+                <footer class="block-group" role="contentinfo">
+                    <jdoc:include type="modules" name="footer" style="footer"/>
+                </footer>
+		    <?php endif; ?>
+        </aside>
+
+        <main role="main">
+	        <?php if ($this->countModules('slideshow')): ?>
+                <section class="slider-container" role="complementary">
+                    <jdoc:include type="modules" name="slideshow" style="flickity"/>
+                </section>
+	        <?php endif; ?>
+
+            <section class="content">
+                <jdoc:include type="message"/>
+                <jdoc:include type="component"/>
+            </section>
+        </main>
+
+    </section>
+</section>
+
+<section class="footer-wrapper block-group">
+	<?php if ($this->countModules('breadcrumbs')): ?>
+        <section class="breadcrumbs-container block-group" role="navigation">
+            <jdoc:include type="modules" name="breadcrumbs"style="none" />
+        </section>
+	<?php endif; ?>
+</section>
+
+<!-- to top -->
+<a href="#" class="go-top"><span class="icon-chevron-up"></span>
+    <p hidden>Top</p></a>
+
+<jdoc:include type="modules" name="debug" style="none"/>
+
+<!-- load scripts -->
+<?php include_once('scripts/scripts.php'); ?>
+
+</body>
+</html>
